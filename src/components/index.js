fdb874d0e7976f9e8c677caf43ea3aeb6221c643
Siddharth7426 <siddharthnegi9821@gmail.com>
2024-12-13 03:28:17 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/Logout";
+import Button from "./Button";
+import Signup from "./Signup";
+import Login from "./Login";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
+import PostCard from  "./postCard"
+import AuthLayout from "./AuthLayout"
+import Input from "./Input"
+import Select from "./Select"
+
+
+export {Header, Footer, Container, Logo, LogoutBtn, Button, Signup, Login, RTE , PostForm, PostCard, AuthLayout, Input, Select}
